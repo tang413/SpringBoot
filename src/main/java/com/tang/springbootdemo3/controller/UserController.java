decae3f87269082e6dbc96a5fab640f28a62fa25
tang <tang868.com>
2021-03-24 18:34:49 +0800

    第一次修改

--- src/main/java/com/tang/springbootdemo3/controller/UserController.java
+++ src/main/java/com/tang/springbootdemo3/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.tang.springbootdemo3.controller;
 
+import com.tang.springbootdemo3.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @RequestMapping("/hello")
+    public User test(){
+        return new User(1, "张", true);
+    }
 }
